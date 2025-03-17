94eb5d1206771ee730fd6d22767e95b58fa82d42
youngpuffy <levimuturi2@gmail.com>
2025-03-17 09:20:47 +0300

    upating scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+ var customerName = 'bob'
+ var bestCustomer;
+const leastFavoriteCustomer = 'levi'
+ function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    
+ }
+ 
+
+function setBestCustomer(){
+    bestCustomer = 'not bob' ;
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'esther'
+}
